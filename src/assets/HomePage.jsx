6522d3e93964ed682493d211a439f1ce6b067eaf
mohame-d2002 <mohamedzigawi@gmail.com>
2026-02-02 15:38:15 +0100

    Update HomePage.jsx

--- src/assets/HomePage.jsx
+++ src/assets/HomePage.jsx
@@ -16,6 +16,7 @@ export function HomePage() {
                     </div>
                     <button className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-300">
                         Get Started
+                        
                     </button>
                 </div>
             </nav>
